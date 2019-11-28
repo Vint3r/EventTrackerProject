a1f554ee8fa4c5c30b4cef0cf2daa94f99ad394a
David Norris <Norris.David.Dev@gmail.com>
2019-11-27 17:28:21 -0700

    Stubbing out initial projects and stuff for weekened project

---
+++ AnimeTrackerJPA/src/main/java/com/skilldistillery/animetracker/AnimeTrackerJpaApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.animetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeTrackerJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimeTrackerJpaApplication.class, args);
+	}
+
+}
